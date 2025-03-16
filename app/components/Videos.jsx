4f55132b06df4fdd5a496aa6dd61906b326ee7e1
AlexManu05 <AlexManu05@users.noreply.github.com>
2025-03-16 02:21:02 +0200

    new song

--- app/components/Videos.jsx
+++ app/components/Videos.jsx
@@ -35,13 +35,14 @@ const VideoCarousel = () => {
         </div>
         <div className="w-full h-[300px] md:w-1/3">
           <iframe
-            className="w-full h-full"
-            src="https://www.youtube.com/watch?si=57dEvK7opqeYtlBU&v=fMu0N5MzX84&feature=youtu.be"
-            title="YouTube video player"
-            frameBorder="0"
+            width="1280"
+            height="743"
+            src="https://www.youtube.com/embed/fMu0N5MzX84"
+            title="Tei goi (Orchestra Version)"
+            frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
           ></iframe>
         </div>
       </div>
